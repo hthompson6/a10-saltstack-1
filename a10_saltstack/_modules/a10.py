2f273e6e5951aad072f1ad6c1923f7d496f78b92
Hunter Thompson <thompson.grey.hunter@gmail.com>
2018-12-27 23:43:47 +0000

    Moved redundant state logic to the execution module

--- a10_saltstack/_modules/a10.py
+++ a10_saltstack/_modules/a10.py
@@ -26,5 +26,122 @@ def __virtual__():
                        'proxy could not be loaded.')
 
 
-def client(host, port, protocol, username, password):
+def _get_client(host, port, protocol, username, password):
     return A10Client(__proxy__['a10.get_session'])
+
+
+def _build_envelope(title, data):
+    return {
+        title: data
+    }
+
+def _to_axapi(key):
+    return translateBlacklist(key, KW_OUT).replace("_", "-")
+
+
+def _build_dict_from_param(param):
+    rv = {}
+
+    for k,v in param.items():
+        hk = _to_axapi(k)
+        if isinstance(v, dict):
+            v_dict = _build_dict_from_param(v)
+            rv[hk] = v_dict
+        if isinstance(v, list):
+            nv = [_build_dict_from_param(x) for x in v]
+            rv[hk] = nv
+        else:
+            rv[hk] = v
+
+    return rv
+
+
+def _validate(**params):
+    # Ensure that params contains all the keys.
+    requires_one_of = sorted([])
+    present_keys = sorted([x for x in requires_one_of if params.get(x)])
+    
+    errors = []
+    marg = []
+    
+    if not len(requires_one_of):
+        return REQUIRED_VALID
+
+    if len(present_keys) == 0:
+        rc,msg = REQUIRED_NOT_SET
+        marg = requires_one_of
+    elif requires_one_of == present_keys:
+        rc,msg = REQUIRED_MUTEX
+        marg = present_keys
+    else:
+        rc,msg = REQUIRED_VALID
+    
+    if not rc:
+        errors.append(msg.format(", ".join(marg)))
+
+
+def _build_json(title, **kwargs):
+    rv = {}
+
+    for x in AVAILABLE_PROPERTIES:
+        v = kwargs.get(x)
+        if v:
+            rx = _to_axapi(x)
+
+            if isinstance(v, dict):
+                nv = _build_dict_from_param(v)
+                rv[rx] = nv
+            if isinstance(v, list):
+                nv = [_build_dict_from_param(x) for x in v]
+                rv[rx] = nv
+            else:
+                rv[rx] = kwargs[x]
+
+    return build_envelope(title, rv)
+
+
+def create(obj_type, **kwargs):
+    payload = build_json(obj_type, **kwargs)
+    try:
+        client = get_client(**kwargs)
+        post_result = client.post(new_url(), payload)
+        ret["changes"].update(**post_result)
+        ret["result"] = True
+    except a10_ex.Exists:
+        ret["result"] = False
+    except a10_ex.ACOSException as ex:
+        ret["comment"] = ex.msg
+        return ret
+    except Exception as gex:
+        raise gex
+    return ret
+
+
+def update(obj_type, **kwargs):
+    payload = build_json(obj_type, **kwargs)
+    try:
+        client = get_client(**kwargs)
+        post_result = client.put(existing_url(**kwargs), payload)
+        ret["changes"].update(**post_result)
+        ret["result"] = True
+    except a10_ex.ACOSException as ex:
+        ret["comment"] = ex.msg
+        return ret
+    except Exception as gex:
+        raise gex
+    return ret
+
+
+def delete(**kwargs):
+    try:
+        client = get_client(**kwargs)
+        client.delete(existing_url(**kwargs))
+        ret["result"] = True
+    except a10_ex.NotFound:
+        ret["result"] = False
+    except a10_ex.ACOSException as ex:
+        ret["comment"] = ex.msg
+        return ret
+    except Exception as gex:
+        raise gex
+    return ret
