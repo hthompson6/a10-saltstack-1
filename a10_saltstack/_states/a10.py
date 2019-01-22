99d7fe94b5fb9abea468834c1f9977267876d1cc
Hunter Thompson <hthompson@a10networks.com>
2019-01-22 22:56:47 +0000

    Abstracted away statefile logic

--- a10_saltstack/_states/a10.py
+++ a10_saltstack/_states/a10.py
@@ -6,78 +6,37 @@
 import salt
 import salt.config
 
-REQUIRED_NOT_SET = (False, "One of ({}) must be set.")
-REQUIRED_MUTEX = (False, "Only one of ({}) can be set.")
-REQUIRED_VALID = (True, "")
 
+def _ret_ops(a10_obj, post_result):
+    ret = dict(
+        name=kwargs['a10_obj'],
+        changes={},
+        original_message="",
+        result=False,
+        comment=""
+    )
 
-# Hacky way of having access to object properties for evaluation
-AVAILABLE_PROPERTIES = ["acl_id","acl_name","arp_disable","description","disable_vip_adv","enable_disable_action","ethernet","extended_stats","ip_address","ipv6_acl","ipv6_address","migrate_vip","name","netmask","port_list","redistribute_route_map","redistribution_flagged","stats_data_action","template_logging","template_policy","template_scaleout","template_virtual_server","use_if_ip","user_tag","uuid","vrid",]
+    ret["changes"].update(**post_result.get('post_resp', {}))
+    if  post_result.get('post_resp'):
+        del post_result['post_resp']
+    ret.update(post_result)
+
+    return ret
 
 
 def create(**kwargs):
-    ret = dict(
-        name=kwargs['a10_obj'],
-        changes={},
-        original_message="",
-        result=False,
-        comment=""
-    )
+    a10_obj = kwargs['a10_obj']
+    post_result = __salt__['a10.create'](a10_obj, **kwargs) 
+    return _ret_ops(a10_obj, post_result)  
 
-    post_result = __salt__['a10.create']("virtual-server",
-                           new_url(),
-                           AVAILABLE_PROPERTIES,
-                           **kwargs) 
-    ret["changes"].update(**post_result['post_resp'])
-    del post_result['post_resp']
-    ret.update(post_result)
-    return ret
-
-
+ 
 def update(**kwargs):
-    ret = dict(
-        name=kwargs['a10_obj'],
-        changes={},
-        original_message="",
-        result=False,
-        comment=""
-    )
-
-    try:
-        post_result = __salt__['a10.update']("virtual-server",
-                               existing_url(**kwargs),     
-                               AVAILABLE_PROPERTIES,
-                               **kwargs)
-        ret["changes"].update(**post_result)
-        ret["result"] = True
-    except a10_ex.ACOSException as ex:
-        ret["comment"] = ex.msg
-        return ret
-    except Exception as gex:
-        raise gex
-    return ret
+    a10_obj = kwargs['a10_obj']
+    post_result = __salt__['a10.update'](a10_obj, **kwargs)
+    return _ret_ops(a10_obj, post_result) 
 
 
 def delete(**kwargs):
-    ret = dict(
-        name="a10_slb_virtual_server",
-        changes={},
-        original_message="",
-        result=False,
-        comment=""
-    )
-
-    try:
-        post_result = __salt__['a10.delete']("virtual-server",
-                               existing_url(**kwargs),      
-                               **kwargs)
-        client.delete(existing_url(**kwargs))
-        ret["result"] = True
-    except a10_ex.NotFound:
-        ret["result"] = False
-    except a10_ex.ACOSException as ex:
-        ret["comment"] = ex.msg
-        return ret
-    except Exception as gex:
-        raise gex
-    return ret
+    a10_obj = kwargs['a10_obj']
+    post_result = __salt__['a10.delete'](a10_obj, **kwargs)
+    return _ret_ops(a10_obj, post_result) 
