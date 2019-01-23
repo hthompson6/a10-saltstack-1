a16fa79e7ea493ae6382cdf6ad31e40c6d3ce01d
Hunter Thompson <hthompson@a10networks.com>
2019-01-23 00:47:24 +0000

    Removed unused modules. Updated licensing

--- a10_saltstack/_modules/a10.py
+++ a10_saltstack/_modules/a10.py
@@ -1,5 +1,16 @@
-# Import Python libraries
-from __future__ import absolute_import, print_function, unicode_literals
+# Copyright 2019 A10 Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 __virtualname__ = 'a10'
 __proxyenabled__ = ['a10']
