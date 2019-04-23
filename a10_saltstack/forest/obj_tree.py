741f5d475b27513605e9af552446cd541d8d66e5
Hunter Thompson <thompson.grey.hunter@gmail.com>
2019-04-23 04:57:52 +0000

    Refactored multiple variable and function names

--- a10_saltstack/forest/obj_tree.py
+++ a10_saltstack/forest/obj_tree.py
@@ -21,69 +21,37 @@ from a10_saltstack.helpers import helper as a10_helper
 from a10_saltstack.forest.nodes import InterNode, ObjNode, RootNode
 
 
-def extract_modname(url):
-    url = url.replace('/axapi/v3/', '').replace('/',
-        '_').replace('-', '_').replace('+', '')
-    url = re.sub(r'_{[a-zA-Z_]+}|{[a-zA-Z_]+}', '', url) 
-    return "a10_{url}".format(url=url)
-
-
-def parse_tree(a10_obj, tree_obj):
+def _extract_modname(url):
     '''
-    Converts the given configuration into a tree using
-    the dfs_cut and dfs_transform helper functions. Creates a
-    root node to which the rest of the configuration tree is attached.
+    Converts the given refrence object url into the name
+    of the corresponding python module.
 
     Args:
-        a10_obj (string):
-        tree_obj (OrderedDict): 
-
-    Returns (object):
-        Root node of the tree 
+        url (string): refrence object url
     '''
-    a10_obj = 'a10_{}'.format(a10_obj)
-
-    
-    root = RootNode(None, a10_obj)
-    altered_tree = dfs_transform(tree_obj)
-
-    root_vals = {}
-    for k,v in altered_tree.items():
-        if k == "a10_name":
-            root.id = v
-        elif k == "a10_obj":
-            root.ref = a10_obj
-        elif type(v) != dict and k != "name":
-            root_vals[k] = v
-
-    root.addValDict(**root_vals)
-
-    root_children = dfs_cut(altered_tree, root)
-
-    if root_children:
-        for child in root_children:
-            root.addChild(child)
-
-    return root 
+    url = url.replace('/axapi/v3/', '').replace('/',
+        '_').replace('-', '_').replace('+', '')
+    module_name = re.sub(r'_{[a-zA-Z_]+}|{[a-zA-Z_]+}', '', url) 
+    return "a10_{mod}".format(mod=module_name)
 
 
-def dfs_cut(obj, refNode=None):
+def _dfs_cut(config, refNode=None):
     '''
-    This iterates over the tree and converts the dictionaries
+    This iterates over the config and converts the dictionaries
     into nodes of a tree.
 
     Args:
-        obj (dict): configuration data
+        config (dict): configuration data
 
     Returns (list):
         List comprised of each node in the tree. Left side has
         the highest nodes and right side has the lowest nodes.
     '''
 
-    if type(obj) != dict:
+    if type(config) != dict:
         return
 
-    vals = list(obj.values())
+    vals = list(config.values())
     if len(vals) == 1 and type(vals[0]) != dict:
         return
 
@@ -93,11 +61,11 @@ def dfs_cut(obj, refNode=None):
         ref_props = {}
 
     node_list = []
-    for k,v in obj.items():
+    for k,v in config.items():
         if k in ref_props:
-            mod_fqdn = extract_modname(ref_props[k])
+            mod_fqdn = _extract_modname(ref_props[k])
             inNode = InterNode(mod_fqdn)
-            child_obj_list = dfs_cut(v, inNode)
+            child_obj_list = _dfs_cut(v, inNode)
             if child_obj_list:
                 for child in child_obj_list:
                     inNode.addChild(child)
@@ -105,7 +73,7 @@ def dfs_cut(obj, refNode=None):
 
         elif type(v) == dict:
             tempNode = ObjNode(k, **v)
-            child_obj_list = dfs_cut(v, refNode)
+            child_obj_list = _dfs_cut(v, refNode)
             if child_obj_list:
                 for child in child_obj_list:
                     tempNode.addChild(child)
@@ -114,26 +82,65 @@ def dfs_cut(obj, refNode=None):
     return node_list
 
 
-def dfs_transform(obj):
+def _dfs_transform(config):
     '''
     This is used to turn the OrderedDicts into normal dicts, and extract lists
     into dicts.
 
     Args:
-        obj (OrderedDict): configuration data
+        config (OrderedDict): configuration data
 
     Returns (dict):
         Configuration data
     '''
-    obj = dict(obj)
-    for k,v in obj.items():
+    config = dict(config)
+    for k,v in config.items():
         new_dict = {}
         if type(v) == list:
            for i in v:
-               new_dict.update(dfs_transform(i))
-           obj[k] = new_dict
+               new_dict.update(_dfs_transform(i))
+           config[k] = new_dict
         if type(v) == OrderedDict:
-           new_dict.update(dfs_transform(v)) 
-           obj[k] = new_dict
+           new_dict.update(_dfs_transform(v)) 
+           config[k] = new_dict
 
-    return obj
+    return config
+
+
+def parse_tree(a10_obj, config):
+    '''
+    Converts the given configuration into a tree using
+    the dfs_cut and dfs_transform helper functions. Creates a
+    root node to which the rest of the configuration tree is attached.
+
+    Args:
+        a10_obj (string):
+        config (OrderedDict): 
+
+    Returns (object):
+        Root node of the tree 
+    '''
+    a10_obj = 'a10_{}'.format(a10_obj)
+
+
+    root = RootNode(None, a10_obj)
+    altered_config = _dfs_transform(config)
+
+    root_vals = {}
+    for k,v in altered_config.items():
+        if k == "a10_name":
+            root.id = v
+        elif k == "a10_obj":
+            root.ref = a10_obj
+        elif type(v) != dict and k != "name":
+            root_vals[k] = v
+
+    root.addValDict(**root_vals)
+
+    root_children = _dfs_cut(altered_config, root)
+
+    if root_children:
+        for child in root_children:
+            root.addChild(child)
+
+    return root
