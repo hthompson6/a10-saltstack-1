55f0b549380027c569b35f8423d7ba970a397122
Hunter Thompson <thompson.grey.hunter@gmail.com>
2019-06-18 21:54:21 +0000

    Added session tests

--- tests/unit/client/test_session.py
+++ tests/unit/client/test_session.py
@@ -11,3 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from mock import Mock, patch
+import unittest
+
+from a10_saltstack.client.session import Session
+
+class TestSession(unittest.TestCase):
+
+    @patch.object(Session, 'authenticate', auto_spec=True)
+    def test_id_auth_called(self, mock_auth):
+        sess = Session(Mock(), 'good_user', 'good_pass')
+        sess.id
+        mock_auth.assert_called_with('good_user', 'good_pass')
+
+    def test_authenticate_accepted(self):
+        pass
+
+    def test_authenticate_rejected(self):
+        pass
+
+    def test_close_no_sessid(self):
+        pass
+
+    def test_close_authorized(self):
+        pass
+
+    def test_close_unuathorized(self):
+        pass
