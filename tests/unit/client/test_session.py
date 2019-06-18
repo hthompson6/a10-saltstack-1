5c637382db8989c95bf08a9c8ead4b97d3f12867
Hunter Thompson <thompson.grey.hunter@gmail.com>
2019-06-18 22:22:19 +0000

    Added auth test

--- tests/unit/client/test_session.py
+++ tests/unit/client/test_session.py
@@ -21,12 +21,18 @@ class TestSession(unittest.TestCase):
 
     @patch.object(Session, 'authenticate', auto_spec=True)
     def test_id_auth_called(self, mock_auth):
-        sess = Session(Mock(), 'good_user', 'good_pass')
-        sess.id
+        Session(Mock(), 'good_user', 'good_pass').id
         mock_auth.assert_called_with('good_user', 'good_pass')
 
-    def test_authenticate_accepted(self):
-        pass
+    @patch.object(Session, 'close', auto_spec=True)
+    def test_authenticate_accepted(self, mock_close):
+        auth_resp = {'authresponse': {'signature': 1337}}
+        attrs = {'post.return_value': auth_resp}
+        mock_http = Mock()
+        mock_http.configure_mock(**attrs)
+        sess = Session(mock_http, Mock(), Mock())
+        sess.authenticate(Mock(), Mock())
+        self.assertEquals('1337', sess.session_id)
 
     def test_authenticate_rejected(self):
         pass
